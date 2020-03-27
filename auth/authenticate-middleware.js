d1dd494cbc4d66e68edb5f738b1516e51d7e7d04
Cameron Moore <cmrnmr@gmail.com>
2020-03-27 15:34:14 -0400

    implemented register, login, auth middleware access to jokes

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,22 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const { authorization } = req.headers;
+  const secret = process.env.JWT_SECRET || 'one ring to rule them all, and in the darkness bind them'
+
+  if(authorization){
+    jwt.verify(authorization, secret, (err, decodedToken) => {
+      if(err){
+        res.status(401).json({"message": "Invalid Credentials"});
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
--- users/users-model.js
+++ users/users-model.js
@@ -1,17 +1,26 @@
-const db = require('../data/dbConfig.js');
+const db = require('../database/dbConfig.js');
 
 module.exports = {
   add,
   find,
+  findBy,
   findById
-}
-
-function find() {
-  return db("users").select('id', 'username');
 }
 
 async function add(user) {
   const [id] = await db("users").insert(user, "id");
 
   return findById(id);
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+function find() {
+  return db("users").select('id', 'username');
+}
+
+function findById(id) {
+  return db("users").where({ id }).select("id", "username", "password").first();
 }
