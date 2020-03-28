0b0480aa48a04fbbe66790d605ebf73b596e8755
Cameron Moore <cmrnmr@gmail.com>
2020-03-28 15:00:37 -0400

    all test verified by red green refactor method

--- api/server.spec.js
+++ api/server.spec.js
@@ -9,36 +9,64 @@ describe('server.js', function() {
   describe('/api/auth/register', function() {
 
     it('should return JSON', function() {
-
-      const user = {"username": "testy mctest", "password": "123"}
-
       return request(server)
-        .post('/api/auth/register').send(user)
+        .post('/api/auth/register').send({"username": "testy mctest", "password": "123"})
         .then(res => {
           expect(res.type).toMatch(/json/i);
         });
     });
 
-    it('should have a property of username', function() {
-      
+    it('should return a status of 200', function() {
       return request(server)
         .post('/api/auth/register').send({"username": "testy mcfly", "password": "121"})
         .then(res => {
-         expect(res.body).toBe({})
+          expect(res.status).toBe(200)
         })
     })
   })
 
   describe('/api/auth/login', function() {
-    it.todo('should send a body containing a username and password')
-    it.todo('should return a token upon providing valid credentials')
+
+    it('should return JSON', function() {
+      return request(server)
+        .post('/api/auth/login').send({"username": "testy mcgee", "password": "124"})
+        .then(res => {
+          expect(res.type).toMatch(/json/i);
+        });
+    });
+
+    it('should return a status of 200', function() {
+      return request(server)
+        .post('/api/auth/login').send({"username": "testy mctest", "password": "123"})
+        .then(res => {
+          expect(res.status).toBe(200)
+        });
+    });
+
   })
 
   describe('/api/jokes', function () {
-    it.todo('should have access to the token')
-    it.todo('should request the token')
-    it.todo('should return jokes if token was valid')
-    it.todo('should return a content header with length of 2010')
+
+    it('should return a status of 200 if accessed with a token', async function() {
+      const auth = await request(server).post('/api/auth/login').send({"username": "testy mctest", "password": "123"})
+
+      const token = auth.body.token
+
+      const response = await request(server).get('/api/jokes').set('authorization', token)
+      expect(response.status).toBe(200)
+    })
+
+    it('should return JSON', async function() {
+      const auth = await request(server).post('/api/auth/login').send({"username": "testy mctest", "password": "123"})
+  
+      const token = auth.body.token
+  
+      const response = await request(server).get('/api/jokes').set('authorization', token)
+      expect(response.type).toMatch(/json/i)
+    })
   })
+
+
+
 
 })
